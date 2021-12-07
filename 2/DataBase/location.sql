bf22f03b91192bdc864736a34ffc44ff4be6b433
Bartoch78 <90573748+Bartoch78@users.noreply.github.com>
2021-12-07 20:05:29 +0100

    Add database

---
+++ 2/DataBase/location.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 07 Gru 2021, 18:24
+-- Wersja serwera: 10.4.17-MariaDB
+-- Wersja PHP: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `sql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `location`
+--
+
+CREATE TABLE `location` (
+  `ID_place` int(11) NOT NULL,
+  `Name_place` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
