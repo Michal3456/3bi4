85f62453296c46b669c9fa5348e8b95f0d374af2
FilipCieczko <91605912+FilipCieczko@users.noreply.github.com>
2022-01-05 14:15:55 +0100

    Update view0.php

--- 4/view/view0.php
+++ 4/view/view0.php
@@ -23,7 +23,26 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
+$sql = "SELECT * FROM gps";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>" $result "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$sql = "SELECT * FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>" $result "<br>";
+    }
+} else {
+    echo "0 results";
+}
 $conn->close();
 ?>
 
